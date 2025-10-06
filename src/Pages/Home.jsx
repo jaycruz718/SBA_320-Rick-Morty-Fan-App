d545dc914e719f937dd6476801d7cfcfcabefa5c
Tiamat <Jaycruz718@gmail.com>
2025-10-06 17:01:47 -0400

    Added background pic to Home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,12 @@
-const Home = () => (
-  <div>
-    <h2>Welcome to the Rick and Morty Fan App</h2>
-    <p>Explore characters, episodes, and locations!</p>
-  </div>
-);
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h2>Welcome to the Rick and Morty Fan App</h2>
+      <p>Explore characters, episodes, and locations from the multiverse!</p>
+    </div>
+  );
+}
 
 export default Home;
